e8ad6746852a7ffde2fb51a2f2457bbdddb926ed
jaredgoldmandrums@gmail.com <jaredgoldmandrums@gmail.com>
2021-03-25 21:10:43 +0000

    added human readable dates

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -38,108 +38,113 @@ $(document).ready(function() {
     }
   }
 
+  const createHumanReadableDate = (date) => {
+    let feedDate = new Date(date).toDateString();
+    return `posted on ${feedDate}`
+  }
 
+  
   const createTweetElement = (tweetObject) => {
     const name = tweetObject.user.name;
     const avatar = tweetObject.user.avatars;
     const handle = tweetObject.user.handle;
     const tweet = tweetObject.content.text;
-    const dateCreated = tweetObject.created_at;
+    const dateCreated = createHumanReadableDate(tweetObject.created_at);
     
     const $tweetHeader = $(
-     `<div class="tweet-top">
-        <div class="avatar">
-        <img src="${avatar}" class="avatar-image">
-        <p>${name}</p>
+      `<div class="tweet-top">
+      <div class="avatar">
+      <img src="${avatar}" class="avatar-image">
+      <p>${name}</p>
       </div>
       <p class="handle">${handle}</p>
       </div>`
-    )
-    
-    const $tweetBody = $('<div class="tweetbody">').text(tweet);
-
-    const $tweetFooter = $(
-      `<div class="tweet-bottom">
-      <div>${dateCreated}</div>
-      <div class="socials">
-      <img src="/images/heart-solid.svg">
-      <img src="/images/retweet-solid.svg">
-      <img src="/images/flag-solid.svg">
-      </div>
-      </div>`
-    )
-    
-    const $tweet = $('<article class="tweet">')
-    
-    $tweet.append($tweetHeader)
-    $tweet.append($tweetBody)
-    $tweet.append($tweetFooter)
+      )
       
-    return $tweet;
-    
-    }
-    
-    const isTweetInvalid = (tweet) => {
-      return (tweet.length > 140 || tweet.length <= 0) 
-    }
-
-    // show error messages 
-    const loadErrors = (tweet) => {
-      if (tweet.length < 140) {
-        $('#error').text('you cannot send a blank tweet');
-        $('#error').slideDown()
-      } else {
-        $('#error').text('your tweet exceeds the character limit')
-        $('#error').slideDown()
+      const $tweetBody = $('<div class="tweetbody">').text(tweet);
+      
+      const $tweetFooter = $(
+        `<div class="tweet-bottom">
+        <div>${dateCreated}</div>
+        <div class="socials">
+        <img src="/images/heart-solid.svg">
+        <img src="/images/retweet-solid.svg">
+        <img src="/images/flag-solid.svg">
+        </div>
+        </div>`
+        )
+        
+        const $tweet = $('<article class="tweet">')
+        
+        $tweet.append($tweetHeader)
+        $tweet.append($tweetBody)
+        $tweet.append($tweetFooter)
+        
+        return $tweet;
+        
       }
-    }
-
-    // EVENT HANDLERS //
-    
-    // slide animation for new tweet form 
-    $('#compose-container', ).on('click', function () {
-      if ($('#new-tweet').css('display') === 'block') {
-        $('#new-tweet').slideUp();           
-      } else {
-        $('#new-tweet').slideDown();   
+      
+      const isTweetInvalid = (tweet) => {
+        return (tweet.length > 140 || tweet.length <= 0) 
       }
-    });
-    
-    // makes form-top button appear and dissapear depending on where user has scrolled
-    $(window).on('scroll', function() {
-      var scroll = $(window).scrollTop();
-      if (scroll >= 500) {
-        $('#button-container').removeClass('button-container');
-        $('#button-container').addClass('button-container-scroll');
-      } else {
-        $('#button-container').removeClass('button-container-scroll');
-        $('#button-container').addClass('button-container');
+      
+      // show error messages 
+      const loadErrors = (tweet) => {
+        if (tweet.length < 140) {
+          $('#error').text('you cannot send a blank tweet');
+          $('#error').slideDown()
+        } else {
+          $('#error').text('your tweet exceeds the character limit')
+          $('#error').slideDown()
+        }
       }
-    })
-    
-    // submit event handler 
-    $('#newtweet').on('submit', function(event) {
-      $('#error').slideUp();
-      event.preventDefault();
-      // loadTweets();
-      const $tweet = $('#tweet-text').val();
-      if (isTweetInvalid($tweet)) {
-        loadErrors($tweet);
-      } else {
-        const $serialTweet = $(this).serialize();
-        sendTweet($serialTweet);
-      }
-    }) 
-
-    $(".button-background").on("click", function() {
-      window.scroll(0, 0);
-    });
-
-    $(".button-background").on("mouseover", function() {
-      $(".formtop-img").attr('src', "/images/angle-double-down-white.png")
-    });
-    $(".button-background").on("mouseout", function() {
-      $(".formtop-img").attr('src', "/images/angle-double-up-solid.png")
+      
+      // EVENT HANDLERS //
+      
+      // slide animation for new tweet form 
+      $('#compose-container', ).on('click', function () {
+        if ($('#new-tweet').css('display') === 'block') {
+          $('#new-tweet').slideUp();           
+        } else {
+          $('#new-tweet').slideDown();   
+        }
+      });
+      
+      // makes form-top button appear and dissapear depending on where user has scrolled
+      $(window).on('scroll', function() {
+        var scroll = $(window).scrollTop();
+        if (scroll >= 500) {
+          $('#button-container').removeClass('button-container');
+          $('#button-container').addClass('button-container-scroll');
+        } else {
+          $('#button-container').removeClass('button-container-scroll');
+          $('#button-container').addClass('button-container');
+        }
+      })
+      
+      // submit event handler 
+      $('#newtweet').on('submit', function(event) {
+        $('#error').slideUp();
+        event.preventDefault();
+        // loadTweets();
+        const $tweet = $('#tweet-text').val();
+        if (isTweetInvalid($tweet)) {
+          loadErrors($tweet);
+        } else {
+          const $serialTweet = $(this).serialize();
+          sendTweet($serialTweet);
+        }
+      }) 
+      
+      $(".button-background").on("click", function() {
+        window.scroll(0, 0);
+      });
+      
+      $(".button-background").on("mouseover", function() {
+        $(".formtop-img").attr('src', "/images/angle-double-down-white.png")
+      });
+      $(".button-background").on("mouseout", function() {
+        $(".formtop-img").attr('src', "/images/angle-double-up-solid.png")
     });
     
     loadTweets();
